429fa4425ca44b83dc2247e5710dd35483038d80
pietro <pietro.peerani@gmail.com>
2024-02-15 10:42:11 +0100

    add: help button

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,29 +1,35 @@
 import { useEffect, useState } from "react";
 import ThemeChanger from "./theme-changer";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
-    const [time, setTime] = useState<string>("");
+  const [time, setTime] = useState<string>("");
 
-    const updateTime = () => {
-      const currentTime = new Date();
-      const hours = currentTime.getHours().toString().padStart(2, "0");
-      const minutes = currentTime.getMinutes().toString().padStart(2, "0");
-      setTime(`${hours}:${minutes}`);
-    };
-  
-    useEffect(() => {
+  const updateTime = () => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours().toString().padStart(2, "0");
+    const minutes = currentTime.getMinutes().toString().padStart(2, "0");
+    setTime(`${hours}:${minutes}`);
+  };
+
+  useEffect(() => {
+    updateTime();
+
+    const intervalId = setInterval(() => {
       updateTime();
-  
-      const intervalId = setInterval(() => {
-        updateTime();
-      }, 10000);
-  
-      return () => clearInterval(intervalId);
-    }, []);
+    }, 10000);
 
-    return (
-        <footer className="flex-shrink-0 pt-2 select-none text-sm">
-            By Pietro Peerani | {time} | <ThemeChanger />
-        </footer>
-    )
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <footer className="flex-shrink-0 pt-2 select-none text-sm flex">
+      <div className="w-full">
+        By Pietro Peerani | {time} | <ThemeChanger />
+      </div>
+      <div className="w-fit whitespace-nowrap max-md:hidden">
+        <Link to="/about" className="text-txLink">:help</Link> for commands
+      </div>
+    </footer>
+  )
 }
