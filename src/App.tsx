7b0f3d60d05ad88a3a4b7b345dbd16993107c553
pietro <pietro.peerani@gmail.com>
2024-11-29 21:43:57 +0100

    fix: gh pages route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-// import { Route, HashRouter as Router, Routes } from "react-router-dom"
+// import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import { Route, HashRouter as Router, Routes } from "react-router-dom"
 import Home from "./pages/home/home"
 import About from "./pages/about/about"
 import Header from "./components/header/header"
 import Navbar from "./components/navbar/navbar"
 import Footer from "./components/footer/footer"
 import Command from "./components/command/command"
+import Skills from "./pages/skills/skills"
 
 export default function App() {
   return (
-    <Router basename="/pietropeerani">
+    <Router>
       <main className="h-screen p-4 pb-2 flex flex-col">
         <section className="relative flex flex-col p-4 pb-2 flex-grow border-2 border-txPrimary">
           <Header />
@@ -17,7 +18,6 @@ export default function App() {
           <div className="mt-4 relative flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/pietropeerani" element={<Home />} />
               <Route path="/about" element={<About />} />
             </Routes>
           </div>
