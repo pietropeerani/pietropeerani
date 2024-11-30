71bf1e05894f448b2442962f58e4cc1125f6a9ac
pietro <pietro.peerani@gmail.com>
2024-11-30 10:56:24 +0100

    update: command

--- src/components/command/command.tsx
+++ src/components/command/command.tsx
@@ -1,19 +1,20 @@
 import { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface commandsProps {
     cmd: string[];
     description: string;
     render?: boolean;
-    action: () => void;
+    action: (navigate: ReturnType<typeof useNavigate>) => void;
 }
 
 export const commands: commandsProps[] = [
     {
-        cmd: ['about'],
+        cmd: ['about', 'help'],
         description: 'Displays information about the app',
         render: true,
-        action: () => {
-            console.log('about');
+        action: (navigate) => {
+            navigate("/about");
         }
     },
     {
@@ -31,10 +32,59 @@ export default function Command() {
     const [showInput, setShowInput] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
     const [errorTimeout, setErrorTimeout] = useState<number | null>(null);
+    const [, setSuggestedPartWidth] = useState(0);
 
     const errorTimer: number = 3;
-
     const inputRef = useRef<HTMLInputElement>(null);
+    const spanRef = useRef<HTMLSpanElement>(null);
+
+    const navigate = useNavigate();
+
+    const handleTabCompletion = () => {
+        const matchingCommands = commands.filter(command =>
+            command.cmd.some(c => c.toLowerCase().startsWith(inputValue.toLowerCase()))
+        );
+
+        const sortedCommands = matchingCommands
+            .map(command => ({
+                ...command,
+                matchedCmd: command.cmd.filter(c => c.toLowerCase().startsWith(inputValue.toLowerCase()))
+            }))
+            .filter(command => command.matchedCmd.length > 0);
+
+        if (sortedCommands.length === 1) {
+            setInputValue(sortedCommands[0].matchedCmd[0]);
+        } else if (sortedCommands.length > 1) {
+            setInputValue(sortedCommands[0].matchedCmd[0]);
+        }
+    };
+
+
+    const getSuggestedPart = () => {
+        const matchingCommands = commands.filter(command =>
+            command.cmd.some(c => c.toLowerCase().startsWith(inputValue.toLowerCase()))
+        );
+
+        if (matchingCommands.length === 1) {
+            const matchingCommand = matchingCommands[0].cmd.find(c => c.toLowerCase().startsWith(inputValue.toLowerCase()));
+
+            if (matchingCommand) {
+                return matchingCommand.slice(inputValue.length);
+            }
+        }
+
+        return '';
+    };
+
+
+
+
+    useEffect(() => {
+        const suggestedText = getSuggestedPart();
+        if (suggestedText && spanRef.current) {
+            setSuggestedPartWidth(spanRef.current.offsetWidth);
+        }
+    }, [inputValue]);
 
     const handleKeyDown = (event: any) => {
         if (event.key === ':' && !showInput && !errorMessage) {
@@ -49,7 +99,7 @@ export default function Command() {
             setShowInput(true);
             setIsEditing(true);
             setInputValue('');
-        } else if (event.key === 'Escape') {
+        } else if (event.key === 'Escape' && !errorMessage) {
             setShowInput(false);
             setIsEditing(false);
             setInputValue('');
@@ -58,11 +108,10 @@ export default function Command() {
             const command = commands.find(cmd => cmd.cmd.some(c => c.toLowerCase() === inputValue.trim().toLowerCase()));
 
             if (command) {
-                command.action();
+                command.action(navigate);
                 setErrorMessage('');
             } else {
                 setErrorMessage(`Comando non trovato: ${inputValue}`);
-                
                 if (errorTimeout) clearTimeout(errorTimeout);
                 const timeout = setTimeout(() => {
                     setErrorMessage('');
@@ -73,11 +122,14 @@ export default function Command() {
             setShowInput(false);
             setIsEditing(false);
             setInputValue('');
-        } else if ((event.key === 'Backspace' || event.key === 'Delete') && inputValue === '') {
+        } else if ((event.key === 'Backspace' || event.key === 'Delete') && inputValue === '' && !errorMessage) {
             setShowInput(false);
             setIsEditing(false);
             setInputValue('');
             setErrorMessage('');
+        } else if (event.key === 'Tab') {
+            event.preventDefault();
+            handleTabCompletion();
         }
     };
 
@@ -97,8 +149,10 @@ export default function Command() {
         };
     }, [isEditing, inputValue, errorMessage, showInput]);
 
+    const inputWidth = spanRef.current ? `${spanRef.current.offsetWidth + 20}px` : 'auto';
+
     return (
-        <div>
+        <div className="relative">
             {errorMessage && (
                 <div className="error-message text-red-500">{errorMessage}</div>
             )}
@@ -110,11 +164,10 @@ export default function Command() {
                             event.preventDefault();
                             const command = commands.find(cmd => cmd.cmd.some(c => c.toLowerCase() === inputValue.trim().toLowerCase()));
                             if (command) {
-                                command.action();
+                                command.action(navigate);
                                 setErrorMessage('');
                             } else {
                                 setErrorMessage(`Comando non trovato: ${inputValue}`);
-                                
                                 if (errorTimeout) clearTimeout(errorTimeout);
                                 const timeout = setTimeout(() => {
                                     setErrorMessage('');
@@ -134,8 +187,20 @@ export default function Command() {
                             value={inputValue}
                             onChange={handleChange}
                             autoFocus
-                            className="ml-2 w-full bg-transparent outline-none border-none"
+                            className="ml-2 w-auto bg-transparent outline-none border-none"
+                            style={{ width: inputWidth }}
                         />
+                        <span ref={spanRef} className="invisible absolute">{inputValue}</span>
+                        {
+                            showInput && !errorMessage && inputValue && (
+                                <div
+                                    className="ml-2 text-gray-500 opacity-50 inline-block"
+                                    style={{ position: 'absolute', left: `${inputWidth}` }}
+                                >
+                                    {getSuggestedPart() ? getSuggestedPart() : ''}
+                                </div>
+                            )
+                        }
                     </form>
                 )
             }
