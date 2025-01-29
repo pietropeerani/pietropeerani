c9d474abcdb58410b19c615c9d8d87821b3099ee
pietro <pietro.peerani@gmail.com>
2025-01-29 15:42:00 +0100

    personal data

--- src/utils/personalData.ts
+++ src/utils/personalData.ts
@@ -3,3 +3,5 @@ export const githubProfile = "https://github.com/pietropeerani"
 export const githubRepo    = "https://github.com/pietropeerani/pietropeerani"
 
 export const instagram     = "https://www.instagram.com/pietro.peerani/"
+
+export const email         = "info@pietropeerani.com"
