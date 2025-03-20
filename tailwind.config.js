a988e05a4221edd63d0fb94389bfebd084d58549
pietro <pietro.peerani@gmail.com>
2025-03-20 18:54:09 +0100

    update: links page infite-animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
       colors: {
         selectionBg: "var(--selection-bg)",
         selectionTx: "var(--selection-tx)",
@@ -27,12 +33,15 @@ export default {
 
         windowBg: "var(--window-bg)",
         windowBorder: "var(--window-border)",
+
+        gramDefault: "var(--gram-default)",
+        gramHover: "var(--gram-hover)",
       },
       fontFamily: {
-        mono: ["JetBrains Mono", "monospace"]
-      }
+        mono: ["JetBrains Mono", "monospace"],
+        serif: ["Inter", "serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
