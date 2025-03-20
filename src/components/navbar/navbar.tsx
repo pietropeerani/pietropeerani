4b3f8f352f9aa99502fe7debb8292e3f3ddc2c5c
pietro <pietro.peerani@gmail.com>
2025-03-20 18:44:58 +0100

    update: navbar links

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,12 +6,24 @@ export default function Navbar() {
 
     const items = [
         {
-            name: "My Projects",
+            name: "Whoami",
             href: "/"
+        },
+        {
+            name: "My Projects",
+            href: "/projects"
+        },
+        {
+            name: "Works",
+            href: "/works"
         },
         {
             name: "Skills",
             href: "/skills"
+        },
+        {
+            name: "Certifications",
+            href: "/certifications"
         },
         {
             name: "About",
@@ -46,16 +58,18 @@ export default function Navbar() {
 
     return (
         <>
-            <div onClick={() => showMenu(!show)} className="hidden gap-2 items-center bg-windowBg px-2 py-1 max-md:flex">
+            <div onClick={() => showMenu(!show)} className="hidden gap-2 items-center bg-windowBg px-2 py-1 max-lg:flex">
                 <div className="cursor-pointer">
-                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.33325 8H26.6666V10.6667H5.33325V8ZM5.33325 14.6667H26.6666V17.3333H5.33325V14.6667ZM26.6666 21.3333H5.33325V24H26.6666V21.3333Z" fill="white" />
+                    <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                        <path className="fill-logo" d="M5.33325 8H26.6666V10.6667H5.33325V8ZM5.33325 14.6667H26.6666V17.3333H5.33325V14.6667ZM26.6666 21.3333H5.33325V24H26.6666V21.3333Z" fill="white" />
                     </svg>
                 </div>
-                <div className="uppercase">Menu</div>
+                <div>
+                    ~{location.pathname}
+                </div>
             </div>
 
-            <div className={`flex max-md:flex-col md:flex-row ${show ? 'block' : 'hidden'} md:block`}>
+            <div className={`flex max-lg:flex-col lg:flex-row ${show ? 'block' : 'hidden'} lg:block`}>
                 {items.map((item, index) => {
                     const isActive = location.pathname === item.href;
                     return (
@@ -64,7 +78,7 @@ export default function Navbar() {
                             to={item.href}
                             className={`text-txLink px-3 py-2 hover:no-underline ${isActive && 'bg-navBg !text-navTx'}`}
                         >
-                            <span className={`max-md:hidden text-txLink ${isActive && 'bg-navBg !text-navTx'}`}>({index})</span> {item.name}
+                            <span className={`max-lg:hidden text-txLink ${isActive && 'bg-navBg !text-navTx'}`}>({index})</span> {item.name}
                         </Link>
                     );
                 })}
